0f8d27642482d95be0fe1c46aa5a44a8cda69d20
adhikaribinod <binod.adhikari@mails.zetalabs.com>
2024-12-26 12:20:11 +0545

    initial commit

---
+++ prisma/migrations/20241209093414_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subject" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "Subject_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
+
+-- AddForeignKey
+ALTER TABLE "Student" ADD CONSTRAINT "Student_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subject" ADD CONSTRAINT "Subject_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.get("/", async (req, res) => {
+  const students = await prisma.student.findMany({
+    include: {
+      Course: true,
+    },
+  });
+  res.json({ data: students });
+});
+app.post("/", async (req, res) => {
+  const course = await prisma.course.create({
+    data: {
+      name: "BCA",
+    },
+  });
+  const students = await prisma.student.create({
+    data: {
+      email: "ram@gmailw.com",
+      name: "ram",
+      password: "haha",
+      courseId: course?.id,
+    },
+  });
+  res.json({ data: "created" });
+});
+
+app.listen(4000, () => {
+  console.log("server running at 4000");
+});
